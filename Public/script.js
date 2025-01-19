777613f46717dcedbaae250d2586b84b71067064
adekorede odebode <adekorede.odebode1@ucalgary.ca>
2025-01-18 18:53:22 -0700

    I have started sytling the nav-bar section

---
+++ Public/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  // This code is for the harmburger menu
